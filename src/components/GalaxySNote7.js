565e27604732a14790dba1af804ac75ac3f1a7ba
marycodes2 <mbeliveau92@gmail.com>
2018-11-27 10:06:56 -0500

    Done.

--- src/components/GalaxySNote7.js
+++ src/components/GalaxySNote7.js
@@ -5,38 +5,44 @@ import exclamation from "../assets/exclamation.png"
 
 
 export default class GalaxySNote7 extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      panicked: false,
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			panicked: false,
+		}
 
-    this.squeelAudio = new Audio(wreee);
-    this.exclaimAudio = new Audio(exclaim);
-    this.exclaimAudio.addEventListener("ended", () => {
-      this.throwAFit()
-    }, false)
-  }
+		this.squeelAudio = new Audio(wreee);
+		this.exclaimAudio = new Audio(exclaim);
+	}
 
-  throwAFit = () => {
-  }
+	throwAFit = () => {
+		this.setState({ panicked: true })
+		setTimeout(() => this.props.alterEnvironment('inhospitable'), 1000)
+	}
 
-  relax = () => {
-  }
+	relax = () => {
+		setTimeout(() => {
+			this.setState({ panicked: false })
+			this.props.alterEnvironment('docile')
+		}, 2000);
+	}
 
-  exclaim = () => {
-    if (this.state.panicked) return
-    this.exclaimAudio.play()
-    this.squeelAudio.play()
-  }
+	exclaim = () => {
+		if (this.state.panicked) return
+		this.exclaimAudio.play()
+		this.squeelAudio.play()
+		this.throwAFit()
+		this.relax()
 
-  panic = () => (<img id="galaxy-exclamation" className="exclamation" src={exclamation} alt="" />)
+	}
 
-  render() {
-    return(
-      <div id="galaxy-s-note" onClick={this.exclaim}>
-        {(this.state.panicked) ? this.panic() : null}
-      </div>
-    )
-  }
+	panic = () => (<img id="galaxy-exclamation" className="exclamation" src={exclamation} alt="" />)
+
+	render() {
+		return (
+			<div id="galaxy-s-note" onClick={this.exclaim}>
+				{(this.state.panicked) ? this.panic() : null}
+			</div>
+		)
+	}
 }
